f2c0ecef8a9a06ca139d69d87242e01786ab8c2a
NacymeG <nacyme.gallel@gmail.com>
2021-10-07 10:15:46 +0200

    Fixed darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -2,7 +2,7 @@ tipsAll = document.querySelector('.tips-all')
 body = document.querySelector('body')
 tipsBtn = document.querySelector('.tips-btn')
 tipsH1 = document.querySelector('.tips-h1')
-tipsStars= document.querySelector('.tips-stars')
+
 function colorMode(toggle) {
     body.style.backgroundColor = (toggle)
      ? '#35363A' 
@@ -19,10 +19,6 @@ function colorMode(toggle) {
     tipsH1.style.color = (toggle) 
     ? 'white'
     :'black';
-
-    tipsStars.style.display = (toggle) 
-    ? 'inline'
-    :'none';
 };
 
 let toggled = true;
